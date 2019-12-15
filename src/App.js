9898c103313ee9942afbb068f3f82548375ba0ed
naderalfakesh <naderalfakesh@gmail.com>
2019-12-15 19:48:18 +0300

    First version of the projects portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Sidebar'
+import Homepage from './Hompage'
+
+// added in order for reducx to work
+import {Provider} from "react-redux";
+import {Switch,Route} from "react-router-dom";
+
+
+
+import {CoinFlipper,BurgerBuilder,TodoList,store,PetsApp} from './Projects'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+    <div className="wrapper">
+      <Sidebar />
+   
+    <div id="content">
+      <Switch>
+        <Route path="/coinflipper">
+          <CoinFlipper />
+        </Route>
+        <Route path="/burgerbuilder">
+        <BurgerBuilder />
+        </Route>
+        <Route path="/todolist">
+        <Provider store={store}>  <TodoList /> </Provider>
+        </Route>
+        <Route path="/petsapp">
+        <PetsApp />
+        </Route>
+        <Route path="/">
+          <Homepage />
+        </Route>
+      </Switch>
+
+    </div>
+
+</div>
+
+
+
     </div>
   );
 }
