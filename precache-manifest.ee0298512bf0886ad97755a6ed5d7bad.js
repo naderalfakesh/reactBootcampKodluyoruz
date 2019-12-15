ff7bf31ed6d47b9d93e8484abdc009825acdafa1
naderalfakesh <naderalfakesh@gmail.com>
2019-12-15 19:46:56 +0300

    Updates

---
+++ precache-manifest.ee0298512bf0886ad97755a6ed5d7bad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187b512d3cb4df7fd4446a2e7cf8cd66",
+    "url": "/reactBootcampKodluyoruz/index.html"
+  },
+  {
+    "revision": "56005c8fa9fc23a9684f",
+    "url": "/reactBootcampKodluyoruz/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "094eecd10fb901495b49",
+    "url": "/reactBootcampKodluyoruz/static/css/main.b2343894.chunk.css"
+  },
+  {
+    "revision": "56005c8fa9fc23a9684f",
+    "url": "/reactBootcampKodluyoruz/static/js/2.8c670ef0.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/reactBootcampKodluyoruz/static/js/2.8c670ef0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "094eecd10fb901495b49",
+    "url": "/reactBootcampKodluyoruz/static/js/main.b7dee3f5.chunk.js"
+  },
+  {
+    "revision": "6d43ec97915d48da3e5e",
+    "url": "/reactBootcampKodluyoruz/static/js/runtime-main.e44f523c.js"
+  },
+  {
+    "revision": "7c42669a738f1a1e15ef7ccedaaa8ed2",
+    "url": "/reactBootcampKodluyoruz/static/media/1-lira-tura.7c42669a.png"
+  },
+  {
+    "revision": "9b70882eb706071cc877c4040c16702d",
+    "url": "/reactBootcampKodluyoruz/static/media/1-lira-yazi.9b70882e.png"
+  }
+]);
